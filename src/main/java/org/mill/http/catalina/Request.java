372104ca1c7c356cdeeeb014d752eeab6ad02727
millzhj <millzhj@163.com>
2014-12-18 15:07:35 +0800

    1

--- src/main/java/org/mill/http/catalina/Request.java
+++ src/main/java/org/mill/http/catalina/Request.java
@@ -26,7 +26,7 @@ public class Request {
 			size = in.read(buffer);
 			while (size != -1) {
 				for (int i = 0; i < size; i++) {
-					sb.append((char) i);
+					sb.append((char) buffer[i]);
 				}
 				if (size >= 2048) {
 					size = in.read(buffer);
